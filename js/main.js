90bcbf2326b784e457edba19ae2f2807bb1cc2cc
NailGozalov <nailgov@gmail.com>
2023-02-06 14:53:34 +0400

    Barbershop V1.0

---
+++ js/main.js
@@ -0,0 +1,40 @@
+let btn_mission = document.querySelector('#btn_mission');
+let btn_why = document.querySelector('#btn_why');
+let btn_about = document.querySelector('#btn_about');
+let mission_text = document.querySelector('.mission_text');
+let why_us_text = document.querySelector('.why_us_text');
+let about_us_text = document.querySelector('.about_us_text');
+
+
+
+
+btn_mission.addEventListener('click', function(){
+    mission_text.style = "display: block;"
+    why_us_text.style = "display: none;"
+    about_us_text.style = "display: none;" 
+
+    btn_mission.style = "background-color: #bb9d52;color: white;"
+    btn_why.style = "background-color: transparent; color: black;"  
+    btn_about.style = "background-color: transparent; color: black;" 
+
+})
+
+btn_why.addEventListener('click', function(){
+    mission_text.style = "display: none;"
+    why_us_text.style = "display: block;"
+    about_us_text.style = "display: none;" 
+
+    btn_mission.style = "background-color: transparent;color: black;"
+    btn_why.style = "background-color: #bb9d52; color: white;"  
+    btn_about.style = "background-color: transparent; color: black;"   
+})
+
+btn_about.addEventListener('click', function(){
+    mission_text.style = "display: none;"
+    why_us_text.style = "display: none;"
+    about_us_text.style = "display: block;"    
+
+    btn_mission.style = "background-color: transparent; color: black;"
+    btn_why.style = "background-color: transparent; color: black;"  
+    btn_about.style = "background-color: #bb9d52;color: white;" 
+})
